22960c8cb82fd4614a32cfbd9c8c37727f8d8294
souta-1326 <souta.tahara@gmail.com>
2021-04-22 20:25:03 +0900

    test

--- mylibrary-without-ACL.cpp
+++ mylibrary-without-ACL.cpp
@@ -24,7 +24,15 @@ template<class T,class S,class R> constexpr inline void input(vector<T> &v,vecto
 }
 template<class T> constexpr inline void input(vector<vector<T>> &v){
   for(int i=0;i<v.size();i++){
-    for(int j=0;j<v[0].size();j++) cin >> v[i][j];
+    for(int j=0;j<v[i].size();j++) cin >> v[i][j];
+  }
+}
+template<class T> constexpr inline void input_graph(vector<vector<T>> &G,int inputcount,const bool isdirect = false,const bool indexed = 1){
+  T a,b;
+  for(int i=0;i<inputcount;i++){
+    cin >> a >> b;a -= indexed;b -= indexed;
+    G[a].emb(b);
+    if(!isdirect) G[b].emb(a);
   }
 }
 template<class T> constexpr inline void output(vector<T> &v,bool space = true){
@@ -44,7 +52,7 @@ template<class T,class S,class R> constexpr inline void output(vector<T> &v,vect
 }
 template<class T> constexpr inline void output(vector<vector<T>> &v){
   for(int i=0;i<v.size();i++){
-    for(int j=0;j<v[0].size()-1;j++) cout << v[i][j] << " ";
+    for(int j=0;j<v[i].size()-1;j++) cout << v[i][j] << " ";
     cout << v[i].back() << endll;
   }
 }
@@ -217,14 +225,14 @@ public:
   constexpr int height(){return V.size();}
   constexpr int width(){return V[0].size();}
   constexpr T &val(int a,int b){return V[a][b];}
-  constexpr vector<T> val(int a){return V[a];}
-  constexpr vector<vector<T>> val(){return V;}
+  constexpr vector<T> &val(int a){return V[a];}
+  constexpr vector<vector<T>> &val(){return V;}
   //ret(mat[i][j],elem(a[i][k],b[k][j]))
   constexpr mat calc(mat &b,function<T(T,T)> ret = [](T x,T y){return x+y;},function<T(T,T)> elem = [](T x,T y){return x*y;})const{
     vector<vector<T>> c(V.size(),vector<T>(b.width()));
     for(int i=0;i<V.size();i++){
-      for(int j=0;j<b.width();j++){
-        for(int k=0;k<b.height();k++) c[i][j] = ret(c[i][j],elem(V[i][k],b.val(k,j)));
+      for(int k=0;k<b.height();k++){
+        for(int j=0;j<b.width();j++) c[i][j] = ret(c[i][j],elem(V[i][k],b.val(k,j)));
       }
     }
     return mat(c);
